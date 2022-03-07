592c93e7c22e075e4a90a0a7069aca9457ca4ab8
EvaMatAb <eva28052426@hotmail.com>
2022-03-07 12:58:35 +0100

    Primera subida archivo java

---
+++ BoletinUD8_B1.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BoletinUD8_B1 {
+
+	public static void main(String[] args) {
+		File f = new File("./Documentos/numeros.txt");
+		
+		try(Scanner leer = new Scanner(f)){
+			int max = leer.nextInt();
+			int min = leer.nextInt();
+			int numero;
+			
+			while(leer.hasNextInt()) {
+				numero = leer.nextInt();
+				
+				if(numero > max) {
+					max = numero;
+				}else if (numero < min) {
+					min = numero;
+				}
+			}
+			
+			leer.close();
+			
+			System.out.println("Número máximo: " + max);
+			System.out.println("Número mínimo: " + min);
+			
+		}catch(FileNotFoundException e) {
+			System.err.println("El archivo no existe.");
+		}catch(NumberFormatException e) {
+			System.err.println("Error. Elemento no numérico.");
+		}
+
+	}
+
+}
